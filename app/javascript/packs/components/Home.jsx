a0b47cd1956e11767102f0085d4b4352961e614a
Faith Oladele <faitholadele@gmail.com>
2020-03-28 02:35:48 -0600

    css check

--- app/javascript/packs/components/Home.jsx
+++ app/javascript/packs/components/Home.jsx
@@ -4,12 +4,30 @@ import '../stylesheets/homepagestyle.scss';
 
 //pry error
 const Home = () => (
- <div  className="card-title headerImage" role="img" alt="image of your choice">
+ <div  className="card-title" 
+ style={{
+ 	backgroundImage: `url("https://res.cloudinary.com/dzv3e4tm4/image/upload/v1585362122/blowballs-close-up-dandelion-132419_b2cqq9.jpg")`,
+  minHeight: '250px',
+  width: '1280px',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  position: 'relative',
+ }}
+ >
  	<div>
- 		<h1 className="center headerImageh1">Queen's Dream Board</h1>
+ 		<h1 className="center black-text">Queen's Dream Board</h1>
  	</div>
-	 <div className="">	
-	  <div className="text-block">
+	 <div>	
+	  <div style={{ 
+	  	position: 'absolute',
+		  bottom: '20px',
+		  right: '20px',
+		  backgroundColor: 'black',
+		  color: 'white',
+		  paddingLeft: '20px',
+		  paddingRight: '20px'
+	  }}>
 	    <h6>Keep building this dream Queen! you're almost there!!!</h6>
 	  </div>
 	 </div>
