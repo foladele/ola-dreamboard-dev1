826db7af1390f03e6ae382277ce28437d9e5c078
Faith Oladele <faitholadele@gmail.com>
2020-04-10 02:25:00 -0600

    images from asw access

---
+++ setupProxy.js
@@ -0,0 +1,10 @@
+const proxy = require("http-proxy-middleware");
+
+module.exports = function(app) {
+	app.use(
+		proxy( "https://ola-dreamboard-dev1.herokuapp.com", {
+			target: "https://ola-dreamboard-dev1.s3.us-east-2.amazonaws.com",
+			changeOrigin: true
+		})
+	);
+}
